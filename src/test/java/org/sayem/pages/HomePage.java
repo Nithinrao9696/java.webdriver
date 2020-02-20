3f469eab5ea790ff90a71aa61200ce40f75ac7ef
Nithinrao9696 <30919697+Nithinrao9696@users.noreply.github.com>
2020-02-20 16:56:54 -0500

    Update HomePage.java

--- src/test/java/org/sayem/pages/HomePage.java
+++ src/test/java/org/sayem/pages/HomePage.java
@@ -1,19 +1,23 @@
-package com.mkyong.examples;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class TestMessageBuilder {
-
+public class GridExample {
+     
     @Test
-    public void testHelloWorld() {
-        assertEquals("hello world", MessageBuilder.getHelloWorld());
-    }
-
-    @Test
-    public void testNumber10() {
-        assertEquals(10, MessageBuilder.getNumber10());
-    }
-
+    public void mailTest() throws MalformedURLException{
+        DesiredCapabilities dr=null;
+        if(browserType.equals("firefox")){
+        dr=DesiredCapabilities.firefox();
+        dr.setBrowserName("firefox");
+        dr.setPlatform(Platform.WINDOWS);
+     
+        }else{
+            dr=DesiredCapabilities.internetExplorer();
+            dr.setBrowserName("iexplore");
+            dr.setPlatform(Platform.WINDOWS);
+        }
+             
+        RemoteWebDriver driver=new RemoteWebDriver(new    URL("http://ap-jenkdx-dev:4444/wd/hub"), dr);
+        driver.navigate().to("http://gmail.com");
+        driver.findElement(By.xpath("//input[@id='Email']")) .sendKeys("username");
+        driver.findElement(By.xpath("//input[@id='Passwd']")) .sendKeys("password");
+        driver.close();
+     
 }

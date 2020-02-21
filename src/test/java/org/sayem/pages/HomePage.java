76a6df9069fa042bafb296c64c6f186bde56c440
Nithinrao9696 <30919697+Nithinrao9696@users.noreply.github.com>
2020-02-20 19:36:27 -0500

    Update HomePage.java

--- src/test/java/org/sayem/pages/HomePage.java
+++ src/test/java/org/sayem/pages/HomePage.java
@@ -1,23 +1,37 @@
-public class GridExample {
-     
+package basicsOfSelenium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class ArrayDemo {
+
     @Test
+
     public void mailTest() throws MalformedURLException{
-        DesiredCapabilities dr=null;
-        if(browserType.equals("firefox")){
-        dr=DesiredCapabilities.firefox();
-        dr.setBrowserName("firefox");
-        dr.setPlatform(Platform.WINDOWS);
-     
-        }else{
-            dr=DesiredCapabilities.internetExplorer();
-            dr.setBrowserName("iexplore");
-            dr.setPlatform(Platform.LINUX);
-        }
-             
-        RemoteWebDriver driver=new RemoteWebDriver(new    URL("http://ap-jenkdx-dev:4444/wd/hub"), dr);
-        driver.navigate().to("http://gmail.com");
-        driver.findElement(By.xpath("//input[@id='Email']")) .sendKeys("username");
-        driver.findElement(By.xpath("//input[@id='Passwd']")) .sendKeys("password");
-        driver.close();
-     
+
+             DesiredCapabilities dr=null;
+
+             dr=DesiredCapabilities.firefox();
+
+             dr.setBrowserName("firefox");
+
+             dr.setPlatform(Platform.LINUX);
+
+             RemoteWebDriver driver=new RemoteWebDriver(new URL("http://ap-jenkdx-dev:4444/wd/hub"), dr);
+
+             driver.navigate().to("https://www.google.co.in");
+
+             driver.findElement(By.xpath("//*[@id='gs_htif0']")) .sendKeys("Search me");
+
+             driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[3]/center/input[1]")) .click();
+
+             driver.close();
+
+}
 }
